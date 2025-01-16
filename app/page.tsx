84eb719e55d27ecaf059e76fa1449ec781ae0e15
Shayan <nwryshayan500@gmail.com>
2025-01-16 10:41:18 +0330

    boro dg

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,17 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import { HomeIcon } from "lucide-react";
-import Image from "next/image";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
         <FloatingNav
-          navItems={[
-            {name: "Home", link: "/", icon: <HomeIcon />}
-          ]}
+          navItems={navItems}
         />
-        {/* <Hero /> */}
-        {/* <Grid /> */}
+        <Hero />
+        <Grid />
         <RecentProjects />
       </div>
     </main>
